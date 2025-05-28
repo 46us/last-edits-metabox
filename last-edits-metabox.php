0424ace4ef2d1a61fc370223c89508c97c6744fa
46us <labandito@gmail.com>
2025-05-28 20:40:46 +0700

    Change require autoload declaration part

--- last-edits-metabox.php
+++ last-edits-metabox.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 
 use LastEditsMetabox\Controllers\LemControllers;
 
